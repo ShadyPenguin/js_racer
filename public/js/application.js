2a7c77f18bd19383c67bc8a0a5051dd6c8c4e406
Jake Sikora <jakesikora.01@gmail.com>
2013-09-24 10:13:33 -0500

    revised player stop, and fixed post method

--- public/js/application.js
+++ public/js/application.js
@@ -1,43 +1,40 @@
 $(document).ready(function(e){
   var game_length = $('tr:first').children().length;
+
   
+
+
   $(document).keyup(function(event){
 
-    if (winner) {
-      $(document).keydown(function(e) {
-        console.log(game_length);
-        if (e.keyCode == 38) {
-          alert('No cheating!');
-        }
-        if (e.keyCode == 32) {
-          alert('No cheating!');
-        }
+    if (winner == false) {
 
-      });
+      if (event.keyCode == 38) {
+        move_player('#player1_strip');
+        player_1_counter += 1;
+      }
+      if (event.keyCode == 32) {
+        move_player('#player2_strip');
+        player_2_counter += 1;
+      }
+      if (player_1_counter == game_length) {
+        winner = $('#p1').text();
+        loser = $('#p2').text();
+        player_1_counter += 1
+        
+        var stats = {winner: winner, loser: loser}
+        $.post('/results', stats, function() {});
+      }
+      else if (player_2_counter == game_length) {
+        winner = $('#p2').text();
+        loser = $('#p1').text();
+        player_2_counter += 1
 
-      var stats = {winner: winner, loser: loser}
-      $.post('/results', stats, function() {});
+        var stats = {winner: winner, loser: loser}
+        $.post('/results', stats, function() {});
+      }
     }
-    if (event.keyCode == 38) {
-      move_player('#player1_strip');
-      player_1_counter += 1;
-    }
-    if (event.keyCode == 32) {
-      move_player('#player2_strip');
-      player_2_counter += 1;
-    }
-    if (player_1_counter == game_length) {
-      winner = $('#p1').text();
-      loser = $('#p2').text();
-      player_1_counter += 1
-    }
-    else if (player_2_counter == game_length) {
-      winner = $('#p2').text();
-      loser = $('#p1').text();
-      player_2_counter += 1
-    }   
 
-  });
+   });
 
 });
  
